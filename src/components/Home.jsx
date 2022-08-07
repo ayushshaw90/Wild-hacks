3dc6741c0db00ea88680d55be81c27c5a311a0a2
Ayush Shaw <ayushshaw127@gmail.com>
2022-08-07 08:58:55 +0530

    modified home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+
 export default function Home() {
+    const [phone, setPhone] = useState("")
+    const phoneApi = 'https://wildhacks-api.herokuapp.com/addphone/'
+    const fetchPost = async (e)=>{
+        console.log(phone)
+        e.preventDefault();
+        const responce = await fetch(phoneApi, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                phno: phone
+            }),
+        })
+        responce.json().then(data=>{
+            console.log(data)
+            let message = data.message;
+            alert(message)
+        })
+    }
     return (
         <div style={{maxWidth: "600px"}} className="mx-auto">
             <div className="py-2 rounded-lg text-gray-200  bg-gradient-to-r from-gray-700 to-zinc-500 px-1">
@@ -14,11 +37,13 @@ export default function Home() {
             </div>
             <div>
                 <div className="px-2 py-4 text-sky-600 font-semibold text-center text-xl ">Subscribe our daily feeds</div>
-                <form className="flex">
+                <form className="flex" onSubmit={(e)=>{fetchPost(e)}}>
                     <label htmlFor="mobile" className="whitespace-nowrap px-2 my-auto h-full bg-gradient-to-r from-green-600 to-teal-600 py-2 rounded-l-lg text-white">
                         Mobile number
                     </label>
-                    <input type="text" className="w-full py-1  px-2"/>
+                    <input type="text" className="w-full py-1  px-2" value={phone} onChange={(e)=>{
+                        setPhone(e.target.value)
+                    }}/>
                     <button className="bg-gradient-to-r from-sky-500 to-cyan-500 text-white px-2 rounded-r-lg">Submit</button>
                 </form>
             </div>
